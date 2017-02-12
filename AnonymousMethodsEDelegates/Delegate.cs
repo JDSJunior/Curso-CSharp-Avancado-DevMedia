11561d75f64f25f388d216cc1a776757544d6b28
José Júnior <junior_dariana@hotmail.com>
2017-02-12 21:56:55 -0200

    second commit

---
+++ AnonymousMethodsEDelegates/Delegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethodsEDelegates
+{
+    //declaração do evento
+    public delegate void PagarEvent(double valor);
+
+    public class Pedido
+    {
+        //evento que dispara o metodo
+        public event PagarEvent Pagar;
+
+        //metodo que aponta para outro
+        public void Fechar(double valor)
+        {
+            this.Pagar(valor);
+        }
+    }
+}
---
+++ AnonymousMethodsEDelegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethodsEDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando a classe 
+            Pedido pedido = new Pedido();
+            //delegação que esta nesse caso atribuindo o metodo
+            pedido.Pagar += delegate(double valor)
+            //metodo anonimo
+            {
+                //assim nao precisa criar o metodo
+                //o codigo só será executado aqui
+                Console.WriteLine("Pagando boleto: " + valor);
+            };
+            //passando o parametro para o metodo
+            pedido.Fechar(2000);
+
+            Console.ReadKey();
+        }
+    }
+}
