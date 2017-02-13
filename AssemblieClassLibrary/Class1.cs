330c79f45b67b998e508568d2b189c59fc45fdfe
José Júnior <junior_dariana@hotmail.com>
2017-02-12 22:01:10 -0200

    third commit

---
+++ AssemblieClassLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿//em Properties -> AssemblyInfo
+//podemos ver informações do assemblie e sua versão
+
+//para versionamento é preciso gerar uma strongKey na aba Assing da propertie do assemblie
+//podemos tbm copiar uma versao do assemblie para o gac(global cache assemblie)
+//evitando de copiar em cada diretorio dos projetos uma dll
+
+using System;
+
+namespace AssemblieClassLibrary.Calculo
+{
+    public static class Class1
+    {
+        public static int Somar(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
