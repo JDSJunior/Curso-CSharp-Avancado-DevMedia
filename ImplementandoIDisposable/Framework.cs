9029b2abd82b3bfb307bbc9f8d5975892466ccb3
José Júnior <junior_dariana@hotmail.com>
2017-02-16 21:03:02 -0200

    tenth commit

---
+++ ImplementandoIDisposable/Framework.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementandoIDisposable
+{
+    public class Conexao : IDisposable
+    {
+        //IDisposable interface que chama o metodo dispose
+        //e limpa os recursos que estão sendo utilizados dentro da classe não 
+        //sao gerenciados pelo Garbage Colector
+        //resource unmanaged pelo GB
+        public string Handle;
+
+        public void Conectar()
+        {
+            //resouce Handle
+            Handle = "Recurso Alocado";
+            Console.WriteLine(Handle);
+        }
+        public void Dispose()
+        {
+            //chama o metodo dispose para liberar recursos
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void LiberarRecursos()
+        {
+            //libera os recursos
+            Handle = String.Empty;
+            Console.WriteLine("Recurso Liberado");
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                LiberarRecursos();
+            }
+        }
+    }
+}
---
+++ ImplementandoIDisposable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ImplementandoIDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //quando sair do bloco using o metodo
+            //dispose é chamado para liberação de recursos
+            using(var con = new Conexao())
+            {
+                con.Conectar();
+            }
+
+
+
+            Console.ReadKey();
+        }
+        private static void TesteSqlConection()
+        {
+            var con = new SqlConnection();
+
+            try
+            {
+                //...
+            }
+            finally
+            {
+                if(con != null)
+                con.Dispose();
+            }
+        }
+    }
+}
