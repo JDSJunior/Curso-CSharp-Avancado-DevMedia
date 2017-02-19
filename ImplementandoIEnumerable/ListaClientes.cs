41e02ddc9adaafaa1fa78ceac2ba9507e939211c
José Júnior <junior_dariana@hotmail.com>
2017-02-18 22:15:22 -0200

    10° commit

---
+++ ImplementandoIEnumerable/ListaClientes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementandoIEnumerable
+{
+    class ListaClientes : IEnumerable
+    {
+        private List<string> _clientes = new List<string>();
+        private int _index = 0;
+
+        public void Add(string str)
+        {
+            //adiciona dinamicamente mais elementos
+            this._clientes.Add(str);
+            this._index++;
+        }
+
+
+
+
+        //public IEnumerable<string> getClientes()
+        //{
+        //   //yield armazena o estado onde parou
+        //   //para pular para o outro return
+        //   yield return this._clientes[0];
+        //   yield return this._clientes[1];
+        //   yield return this._clientes[2];
+        //   yield return this._clientes[3];
+        //}
+
+        //metodo da implementacao da interface IEnumerable
+        //permite varer a classe com foreach diretamente
+        public IEnumerator GetEnumerator()
+        {
+            for(int i = 0; i < _index; i++ )
+                yield return this._clientes[i];
+        }
+    }
+}
---
+++ ImplementandoIEnumerable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementandoIEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clist = new ListaClientes();
+            clist.Add("Bilbo");
+            clist.Add("Júnior");
+            clist.Add("Dariana");
+            clist.Add("José");
+            clist.Add("Loreni");
+
+            //atraves da implementação podemos varrer diretamente na classe
+            //usando um foreach
+            foreach (var item in clist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
