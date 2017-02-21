9d6d5da7aee90bbcd161d68d27f1033816540c14
José Júnior <junior_dariana@hotmail.com>
2017-02-21 20:06:50 -0300

    16° commit

---
+++ LambdaEDelelegates/Delegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaEDelelegates
+{
+
+    //delegate/contrato do evento
+    public delegate void PagarEvent(double valor);
+
+    public class Boleto
+    {
+        //evento que dispara o metodo
+        public event PagarEvent Pagar;
+
+        //passando o evento como parametro
+        public void Fechar(double valor)
+        {
+            this.Pagar(valor);
+        }
+    }
+}
---
+++ LambdaEDelelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaEDelelegates
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Boleto bt = new Boleto();
+            //expressão lambda passada para o evento que aponta para o metodo
+            //o compilador identifica automaticamente o tipo do parametro
+            bt.Pagar += valor => Console.WriteLine("Pagando boleto no valor: " + valor);
+            bt.Fechar(5000);
+
+            Console.ReadKey();
+        }
+    }
+}
