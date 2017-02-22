ec45770dc36dab5870729aa0bfa66e36847dd114
José Júnior <junior_dariana@hotmail.com>
2017-02-22 14:29:55 -0300

    18° commit

---
+++ MetodosAssesores/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosAssesores
+{
+    class Cliente
+    {
+        private string _cliente;
+        private double _valor;
+
+        //metodo tradicional de acesso aos fields
+        public string getCliente()
+        {
+            return this._cliente;
+        }
+        public void setCliente(string cliente)
+        {
+            this._cliente = cliente;
+        }
+        public void Sacar(double saque)
+        {
+            //operador de atribuição decremental
+            //this._valor -= saque;
+
+            this._valor = this._valor - saque;
+        }
+        public void Depositar(double deposito)
+        {
+            //operador de atribuicao incremental
+            //this._valor += deposito;
+            this._valor = this._valor + deposito;
+        }
+        public void Saldo()
+        {
+            Console.WriteLine(this._valor);
+        }
+    }
+}
---
+++ MetodosAssesores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosAssesores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.setCliente("Junior");
+            cliente.Depositar(1000);
+            cliente.Saldo();
+
+            Console.WriteLine(cliente.getCliente());
+            cliente.Sacar(500);
+            cliente.Saldo();
+
+            Console.ReadLine();
+        }
+    }
+}
