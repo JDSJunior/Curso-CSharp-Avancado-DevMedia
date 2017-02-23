c2631bcc230dbc17083cdec3ebec60a00c6e80b5
José Júnior <junior_dariana@hotmail.com>
2017-02-23 18:56:54 -0300

    22° commit

---
+++ OverloadDeMetodos/CaixaDialogo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadDeMetodos
+{
+    class CaixaDialogo
+    {
+        //overload de metodo
+        //varios metodos com o mesmo nome com parametros diferentes
+        public void Show(string Mensagem)
+        {
+            Console.WriteLine(Mensagem);
+        }
+
+        public void Show(string Mensagem, int Delay)
+        {
+
+            //chamando um metodo sobrecarregado dentro de outro
+            //evita a repetição de codigo
+
+            //configuração do delay
+            Show(Mensagem);
+            //Console.WriteLine(Mensagem);
+        }
+        public void Show(string Mensagem, int Delay, bool BotaoOK)
+        {
+            //configuração do delay e BotaoOK
+            Show(Mensagem, Delay);
+            //Console.WriteLine(Mensagem);
+        }
+
+    }
+}
---
+++ OverloadDeMetodos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadDeMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CaixaDialogo cx = new CaixaDialogo();
+
+            //chamada do metodo Show com seus varios overloads
+            cx.Show("Ola mundo");
+            cx.Show("Ola Mundo", 10);
+            cx.Show("ola Mundo", 10, true);
+
+            Console.ReadKey();
+        }
+    }
+}
