1104bb8b624ff868273625f47e9ae229fba9fb60
José Júnior <junior_dariana@hotmail.com>
2017-02-23 18:56:19 -0300

    21° commit

---
+++ Namespaces/Assemblie.cs
@@ -0,0 +1,18 @@
+﻿//assemblie 1 contendo namespace igual ao assemblie2
+//ocorre a divisao mas nao lógica entre namespace de nomes iguais
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Junior.form.button
+{
+    public static class GUID
+    {
+        public static string GeraGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Namespaces/Assemblie2.cs
@@ -0,0 +1,18 @@
+﻿//assemblie2 contendo namespace igual ao assemblie1
+//ocorre a divisao mas nao lógica entre namespace de nomes iguais 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Junior.form.button
+{
+    public static class Data
+    {
+        public static string GeraData()
+        {
+            return DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToLongTimeString();
+        }
+    }
+}
