b727dbd2cbd92ca45ccd0a361c0c92603c42f22b
José Júnior <junior_dariana@hotmail.com>
2017-02-23 18:57:34 -0300

    23° commit

---
+++ OverrideDeMetodos/Arquivo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideDeMetodos
+{
+    class Arquivo
+    {
+        //classe com metodo override que será sobreescrito nas classes derivadas
+        //virtual para parametros completamente iguais
+        //abstratc para parametros diferentes
+
+        public string Nome;
+        public int Tamanho;
+        public virtual void Abrir()
+        {   
+            Console.WriteLine("Abrindo.....{0}", Nome);
+        }
+
+    }
+
+    //classe que herda de arquivo e sobreescreve o metodo virtual
+    class Musica : Arquivo
+    {
+        public override void Abrir()
+        {
+            Console.WriteLine("Abrindo musica: " + Nome);
+            Console.WriteLine("Tamanho da Musica: " + Tamanho +"MB");
+        }
+    }
+
+    class Txt : Arquivo
+    {
+        public override void Abrir()
+        {
+            base.Abrir();
+        }
+    }
+
+    class Imagen : Arquivo
+    {
+        public override void Abrir()
+        {
+            Console.WriteLine("imagem: " + Nome);
+        }
+    }
+
+    class Windows
+    {
+        //lista de arquivos
+        private List<Arquivo> _arquivo;
+        public Windows()
+        {
+            _arquivo = new List<Arquivo>();
+        }
+
+        //adiciona arquivos na lista
+        public void Add(Arquivo _item)
+        {
+            _arquivo.Add(_item);
+        }
+
+        //abre todos os arquivos
+        //passando a lista como parametro
+        public void AbrirTodos()
+        {
+            foreach(Arquivo arquivo in _arquivo)
+            {
+                Abrir(arquivo);
+            }
+        }
+
+        //delegação
+        // abre os arquivos individualmente
+        public void Abrir(Arquivo arquivo)
+        {
+            //se o arquivo é Musica
+            //if (arquivo is Musica)
+            //passe arquivo como musica e use o metodo Abrir();
+                //(arquivo as Musica).Abrir();
+            //if (arquivo is Imagen)
+                //(arquivo as Imagen).Abrir();
+            //if (arquivo is Txt)
+                //(arquivo as Txt).Abrir();
+
+            //metodo da classe base
+            arquivo.Abrir();
+        }
+    }
+}
---
+++ OverrideDeMetodos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideDeMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arquivo texto = new Txt() { Nome = "relatorio", Tamanho =  10};
+            Arquivo mp3 = new Musica(){Nome = "Qualquer" , Tamanho = 20};
+            Arquivo foto = new Imagen() { Nome = "ferias", Tamanho = 5};
+
+            Windows windows = new Windows();
+            //adiciona os arquivos na lista
+            windows.Add(mp3);
+            windows.Add(texto);
+            windows.Add(foto);
+
+            //abre todos os arquivos
+            windows.AbrirTodos();
+
+            Console.ReadKey();
+
+        }
+    }
+}
