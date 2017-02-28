926b768246aae3affd0e2f24b300e8885ea4d385
José Júnior <junior_dariana@hotmail.com>
2017-02-27 23:34:23 -0300

    24° commit

---
+++ ParametrosNomeados/Dialogo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosNomeados
+{
+    public class DialogoBad
+    {
+        //bad smell longer parameters list
+        public void Show(string Mensagem, bool MostrarBotaoOk, bool MostarBotaoYes, bool MostarBotaoCancelpublic, int Delay)
+        {
+            Console.WriteLine(Mensagem);
+        }
+    }
+
+    public class Dialogo
+    {
+        //usa uma instancia da classe com os parametros para o metodo
+        public void Show(DialogoParams parameters)
+        {
+            Console.WriteLine(parameters.Mensagem);
+        }
+    }
+
+    //encapsula os parametros para evitar o bad smell longer parameters list
+    public class DialogoParams
+    {
+        public string Mensagem;
+        public bool MostrarBotaoOk;
+        public bool MostarBotaoYes;
+        public bool MostarBotaoCancel;
+        public int Delay;
+        //assim é possivel adicionar e alterar parametros sem um efeito domino 
+        //nas chamadas de metodo
+        public int Modal;
+    }
+}
---
+++ ParametrosNomeados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosNomeados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dialogo dg1 = new Dialogo();
+            //metodo tradicional de passar parametros como variaveis
+            //string mensagem = "Confirmar Operação?";
+            //bool mostrarbotaook = true;
+            //bool mostrarbotaoyes = true;
+            //bool mostrarbotaocancel = false;
+            //int delay = 10;
+            //dg1.Show(mensagem, mostrarbotaook, mostrarbotaoyes, mostrarbotaocancel, delay);
+
+            //passando parametros nomeados sem nescessidade de passar na ordem
+            //dg1.Show(Mensagem: "Confirmar Operação?", MostrarBotaoOk: true, MostarBotaoYes: true, MostrarBotaoCancel: false, Delay: 10);
+
+            //cria uma instancia da classe que contem os parametros encapsulados 
+            //para passar na chamada do metodo
+            var parametros = new DialogoParams()
+            {
+                Mensagem = "Confirmar Operação?",
+                MostrarBotaoOk = true,
+                MostarBotaoYes = true,
+                MostarBotaoCancel = false,
+                Delay = 10
+            };
+            dg1.Show(parameters: parametros);
+
+            Console.ReadKey();
+        }
+    }
+}
