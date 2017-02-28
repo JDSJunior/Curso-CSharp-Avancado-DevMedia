6c5420f89a1bca7e2fbbb0b4c011024045e02b59
José Júnior <junior_dariana@hotmail.com>
2017-02-27 23:35:07 -0300

    25° commit

---
+++ ParametrosOpcionais/CaixaDialogo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosOpcionais
+{
+    class CaixaDialogo
+    {
+        //passando um valor diretamente para o parametro
+        //o compilador atribuira o valor caso seja omitido a parametro na chamada
+        //do metodo
+        //os parametros opcionais devem ser declarados depois de todos os 
+        //parametros
+        public void Show(string Mensagem, int Delay = 10)
+        {
+            Console.WriteLine(Mensagem);
+        }
+    }
+}
---
+++ ParametrosOpcionais/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosOpcionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CaixaDialogo cx = new CaixaDialogo();
+            //chamando o metodo e omitindo o parametro Delay que recebe
+            //valor que foi atribuido na assinatura so metodo na classe base
+            cx.Show("Ola mundo");
+            Console.ReadKey();
+        }
+    }
+}
