9cafe49e64fdd7c0880a03056c83769ced6fb964
José Júnior <junior_dariana@hotmail.com>
2017-03-04 20:16:32 -0300

    30° commit

---
+++ PropriedadesIndexadas/Produtos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropriedadesIndexadas
+{
+    class Produtos
+    {
+        private List<string> itens;
+
+        public Produtos()
+        {
+            //construtor que inicializa a lista
+            this.itens = new List<string>();
+        }
+
+        //metodo que adiciona um valor a lista
+        public void Add(string iten)
+        {
+            this.itens.Add(iten);
+        }
+
+        //retorna um valor usando o metodo get da lista
+        public string Get(int index)
+        {
+            return this.itens[index];
+        }
+
+
+        //propriedade indexada
+        public string this[int index]
+        {
+            //usando o get para retornar os valores pelo indice
+            get
+            {
+                //pode ser usado o metodo get
+                //return this.Get(index);
+                return this.itens[index];
+            }
+
+            //pode ser implementado o set também
+            //set { };
+        }
+    }
+}
---
+++ PropriedadesIndexadas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropriedadesIndexadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produtos prod = new Produtos();
+
+            //adicionando valores a lista
+            prod.Add("Celular");
+            prod.Add("Tablet");
+            prod.Add("SmartPhone");
+
+            //obtendo os valores atraves do metodo get da List
+            Console.WriteLine(prod.Get(0));
+            Console.WriteLine(prod.Get(1));
+            Console.WriteLine(prod.Get(2));
+
+            //obtendo os elementos da lista atraves da propriedade indexada
+            Console.WriteLine(prod[0]);
+            Console.WriteLine(prod[1]);
+            Console.WriteLine(prod[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
