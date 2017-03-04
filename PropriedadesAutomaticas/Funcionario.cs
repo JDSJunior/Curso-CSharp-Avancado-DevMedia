0c47c22608badfb6ce9f354924fda334332e3233
José Júnior <junior_dariana@hotmail.com>
2017-03-04 20:15:48 -0300

    29° commit

---
+++ PropriedadesAutomaticas/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropriedadesAutomaticas
+{
+    class Funcionario
+    {
+        //propriedades automaticas 
+        //metodos gerados em tempo de execucão pelo compilador
+
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+    }
+}
---
+++ PropriedadesAutomaticas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropriedadesAutomaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            
+            f.Codigo = 123;
+            f.Nome = "junior";
+            f.Salario = 10000;
+        }
+    }
+}
