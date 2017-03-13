b243ad14ce34319e0be9cd14cb9ab9ba5f9dc4af
José Júnior <junior_dariana@hotmail.com>
2017-03-13 20:55:18 -0300

    37° commit

---
+++ ValueRef_2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueRef_2
+{
+    public class Cliente
+    {
+        private int codigo;
+        private string nome;
+        private string telefone;
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value;  }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+
+        //sobrescrevendo o metodo ToString() da classe object
+        //para imprimir os dados da classe
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Codigo: " + this.Codigo.ToString());
+            sb.AppendLine("Nome: " + this.Nome);
+            sb.AppendLine("Telefone: " + this.Telefone);
+
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimeTypeValue();
+
+            ImprimeTypeReference();
+
+            ApontaParaMesmaPosicao();
+        }
+
+        private static void ApontaParaMesmaPosicao()
+        {
+            Cliente c1 = new Cliente();
+            Cliente c2 = new Cliente();
+
+            //aponta para a mesma posiçao de memoria
+            c2 = c1;
+
+            //compara os tipos de objetos
+            if (c2.Equals(c1))
+            {
+                Console.WriteLine("Objetos Iguais");
+            }
+            else
+            {
+                Console.WriteLine("Objetos Diferentes");
+            }
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+            Console.ReadKey();
+        }
+
+        private static void ImprimeTypeReference()
+        {
+            Cliente c = new Cliente();
+
+            //type reference imprime o namespace e o nome do objeto
+
+            Console.WriteLine(c);
+
+            c.Codigo = 132;
+            c.Nome = "junior";
+            c.Telefone = "99999999";
+
+            Console.WriteLine(c.Codigo);
+            Console.WriteLine(c.Nome);
+            Console.WriteLine(c.Telefone);
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+
+        private static void ImprimeTypeValue()
+        {
+            int x = 10;
+            string s = "junior";
+            bool b = true;
+
+            //type valor imprime o valor atribuido em cada variavel
+            Console.WriteLine(x);
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ValueRef_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ValueRef_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValueRef_2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d3a6008-11dc-4a4e-bcd1-26db8a029297")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
